2bfe9766582f6e5fb02e0a64732d1a6ccb313375
Victor <67005852+jeeest@users.noreply.github.com>
2021-06-18 14:52:55 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
   });
 });
 
-app.use(`/test`, router);
+app.use(`/.netlify/functions/test`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
